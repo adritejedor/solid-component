3ea7b5bcda8ca0a7084ea8843a196a47131b6281
adrian <adriantejedor96@gmail.com>
2020-07-08 10:36:25 +0200

    añadida fuente para el minutero

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent implements OnInit {
+export class DisplayComponent implements OnInit, OnChanges {
 
   @Input() time:number = null;
   
+  public minutes: string = "00";
+  public seconds: string = "00";
+
   constructor() { }
+
+  ngOnChanges(changes) {
+    if (changes.time) {
+      const minutes = Math.trunc(changes.time.currentValue / 60);
+      const seconds = changes.time.currentValue - minutes * 60;
+      this.minutes = ("0" + minutes).substr(-2);
+      this.seconds = ("0" + seconds).substr(-2);
+    }
+  }
 
   ngOnInit(): void {
   }
